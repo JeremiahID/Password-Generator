fecd070571ee518114caaefada80a33074abfb90
Idahosa Jeremiah Efe <113017684+JeremiahID@users.noreply.github.com>
2023-04-15 19:51:04 +0100

    Add files via upload

---
+++ password check.py
@@ -0,0 +1,8 @@
+password=""
+while not password:
+    password=input=("enter a valid password: ")
+    if password=="jerry":
+        print("password successful")
+    else:
+        print("please enter a valid password")
+        password=""
